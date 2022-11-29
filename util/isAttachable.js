62ea793bfb855e44b90ed180827b8bb36bceeec7
eldossjogy <eldossjogy@gmail.com>
2022-11-29 17:43:12 -0500

    Added .mov as an attachable

--- util/isAttachable.js
+++ util/isAttachable.js
@@ -1,5 +1,5 @@
 const isAttachable = async function (media) {
-  isVideo = media => (/\.(mp4|3gp|ogg|wmv|webm|flv|avi*|wav|vob*)$/i).test(media)
+  isVideo = media => (/\.(mp4|3gp|mov|ogg|wmv|webm|flv|avi*|wav|vob*)$/i).test(media)
   isImage = media => (/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).test(media)
   extension = media => media.match(/\.[0-9a-z]+$/i)[0]
 
